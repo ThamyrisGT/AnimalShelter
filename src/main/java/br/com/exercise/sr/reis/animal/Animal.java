06e6d1d9072277a3a39a9755ca680330386a90f5
Thamyris <tgthurler@gmail.com>
2022-05-24 20:00:26 -0300

    Added attribute adopted in animal entity

--- src/main/java/br/com/exercise/sr/reis/animal/Animal.java
+++ src/main/java/br/com/exercise/sr/reis/animal/Animal.java
@@ -8,8 +8,9 @@ public class Animal {
 	private String code;
 	private String size;
 	private String type;
+	private Boolean adopted;
 
-	public Animal(Double weight, String name, Integer age, String code, String size, String type) {
+	public Animal(Double weight, String name, Integer age, String code, String size, String type, Boolean adopted) {
 		super();
 		this.weight = weight;
 		this.name = name;
@@ -17,35 +18,35 @@ public class Animal {
 		this.code = code;
 		this.size = size;
 		this.type = type;
+		this.adopted = adopted;
 	}
 
 	public Double getWeight() {
 		return weight;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public Integer getAge() {
 		return age;
 	}
 
-
 	public String getCode() {
 		return code;
 	}
-
 
 	public String getSize() {
 		return size;
 	}
 
-
 	public String getType() {
 		return type;
+	}
+
+	public Boolean getAdopted() {
+		return adopted;
 	}
 
 }
